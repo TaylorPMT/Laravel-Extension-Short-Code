387320f51714c0c1e6553ee82dc98cabcfe83af5
TimoCBA <timo@cybridge.jp>
2022-04-06 19:33:05 +0900

    first commit

---
+++ src/Facades/Shortcode.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Taylorpmt\ShortCode\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class Shortcode extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'shortcode';
+    }
+}
